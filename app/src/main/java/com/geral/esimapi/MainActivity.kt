b42d84ea7f95b06d00457616629bfcc5271ffc36
Daniel <dleguizamon@merqueo.com>
2023-03-16 10:31:22 -0500

    nuevos cambios

--- app/src/main/java/com/geral/esimapi/MainActivity.kt
+++ app/src/main/java/com/geral/esimapi/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.geral.esimapi
 
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.telephony.euicc.DownloadableSubscription
 import android.telephony.euicc.EuiccInfo
 import android.telephony.euicc.EuiccManager
 import android.util.Log
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.geral.esimapi.databinding.ActivityMainBinding
 
@@ -19,6 +23,7 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -27,6 +32,31 @@ class MainActivity : AppCompatActivity() {
             this.getSystemService(Context.EUICC_SERVICE) as EuiccManager
 
         // es compatible
+        val activationCode = "1234-5678-9012-3456" // código de activación de la eSIM
+        val confirmationCode = "123456" // código de confirmación de la eSIM
+
+        val request = DownloadableSubscription.Builder(activationCode)
+            .setEncodedActivationCode(activationCode)
+            .setConfirmationCode(confirmationCode)
+            .build()
+
+        val intent = Intent(action).setPackage(applicationContext.packageName)
+        val act = applicationContext.packageManager.resolveActivity(intent, 0)
+        intent.setClassName(
+            act?.activityInfo?.packageName.orEmpty(),
+            act?.activityInfo?.name.orEmpty()
+        )
+
+        euiccManager.downloadSubscription(
+            request,
+            true,
+            PendingIntent.getService(
+                this,
+                1,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+        )
         Toast.makeText(this, "${euiccManager.isEnabled}", Toast.LENGTH_SHORT).show()
         // hardware eUICC y la versión del sistema operativo eSIM
         val info: EuiccInfo? = euiccManager.euiccInfo
